53367427ae3433acc1807f183270483c9f939c43
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2025-10-29 12:02:49 +0100

    chore: add netlify redirect

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import AuthLayout from '../layouts/auth/Index';
 import Auth from '@/pages/auth/auth';
 import ForgotPassword from '@/pages/auth/forgot-password';
@@ -6,6 +6,10 @@ import VerifyAccount from '@/pages/auth/verify-account';
 import ResetPassword from '@/pages/auth/reset-password';
 import ResetPasswordSuccess from '@/pages/auth/reset-password-success';
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/auth" replace />,
+  },
   {
     path: '/auth',
     element: <AuthLayout />,
---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import eslintConfig from '@uhomes/eslint-config';
+
+export default eslintConfig;
