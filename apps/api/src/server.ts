307104ebcc56698baece146e055a45ff9a8c13f8
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2025-11-11 12:15:43 +0100

    chore: remove unused imports in server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,20 +1,17 @@
-import 'dotenv/config'
-import https from 'https'
-import fs from 'fs'
-import app from './app'
-import connectDB from './config/db'
+import 'dotenv/config';
 
-const PORT = process.env.PORT || 7000
+import app from './app';
+import connectDB from './config/db';
 
+const PORT = process.env.PORT || 7000;
 
-  connectDB()
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(`HTTP Server running at https://localhost:${PORT}`)
-      })
-    })
-    .catch((error) => {
-      console.error('Failed to connect to the database', error)
-      process.exit(1)
-    })
-
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`HTTP Server running at https://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database', error);
+    process.exit(1);
+  });
