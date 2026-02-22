fed0bd27018b2a8deab7ba49faccef730469d7b3
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2026-02-22 21:31:47 +0100

    feat: improve signup error

--- apps/web/src/pages/auth/sign-up.tsx
+++ apps/web/src/pages/auth/sign-up.tsx
@@ -75,10 +75,9 @@ const Signup = () => {
       navigate('/auth/verify-account');
     } catch (error) {
       if (error instanceof AxiosError) {
+        console.log(error.response?.data.data.error);
         const errorMessage =
-          error.response?.data?.error ||
-          error.response?.data?.message ||
-          'Registration failed. Please try again.';
+          error.response?.data?.data.error || 'Registration failed. Please try again.';
         toast.error(errorMessage);
       } else {
         toast.error('An unexpected error occurred. Please try again.');
